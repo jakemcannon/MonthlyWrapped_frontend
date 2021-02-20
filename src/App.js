47a58ff6adc1f1fca1e36999be424feda1078026
Jacob Cannon <tug33296@temple.edu>
2021-02-19 22:30:34 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Create monthly Instagram stories based on your Spotify listening trends
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
