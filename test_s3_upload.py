d66c9c6036aac382d159794009d332cb440b4151
Jacob Cannon <tug33296@temple.edu>
2021-03-05 22:02:39 -0500

    setup config file

--- test_s3_upload.py
+++ test_s3_upload.py
@@ -1,9 +1,12 @@
+import config
+
 import boto3
 
+bucket_name = config.bucket_name
 local_file_name = 'test_from_local2.jpg'
 remote_s3_file_name = 'test.jpg'
 
-client = boto3.client('s3', aws_access_key_id = os.environ.get("PUBLIC_KEY"), aws_secret_access_key = os.environ.get("PUBLIC_KEY"))
+client = boto3.client('s3', aws_access_key_id = config.api_key, aws_secret_access_key = config.api_secret)
 
 def upload(file_name, bucket, object_name):
 
@@ -14,7 +17,7 @@ def upload(file_name, bucket, object_name):
 def download(bucket, file_name):
 	print(file_name)
 	print(f'./{file_name}')
-	response = client.download_file(bucket_name, file_name, f'./{file_name}')
+	response = client.download_file(bucket_name, file_name, f'./media/{file_name}')
 	return response
 
 
