d4cb350dabd43eafb59c9caf2b0777dcf022f2a3
Jacob Cannon <tug33296@temple.edu>
2021-03-07 22:45:22 -0500

    add classes for generating images

---
+++ settings.py
@@ -0,0 +1,67 @@
+from time import strftime
+
+BASE_COLOR = "#F1C3ED"
+HEADER_FONT_COLOR = (0,0,0)
+FOOTER_FONT_COLOR = (0,0,0)
+
+HEADER_FONT_SIZE = 70
+FOOTER_FONT_SIZE = 60
+
+HEADER_TEXT = f"{strftime('%B')} Top ArtistsSS"
+FOOTER_TEXT = "monthlywrapped.com"
+
+HEADER_FONT = 'Library/Fonts/CircularStd-Bold.ttf'
+FOOTER_FONT = 'Library/Fonts/CircularStd-Bold.ttf'
+
+HEADER_POSITION_HEIGHT = 107
+FOOTER_POSITION = (231, 1695)
+
+
+"""
+SongStory
+"""
+
+SONG_STORY_FONT_COLOR = (0, 0, 0)
+SONG_STORY_IMAGE_SIZE = (128, 130)
+SONG_TEXT_FONT = 'Library/Fonts/CircularStd-Medium.ttf'
+SONG_TEXT_SIZE = 50
+ARTIST_TEXT_FONT = 'Library/Fonts/CircularStd-BookItalic.ttf'
+ARTIST_TEXT_SIZE = 30
+
+
+SONG_THUMBNAIL_POSITION = [
+	(854, 200), (854, 352),
+	(854, 504), (854, 656),
+	(854, 808), (854, 960),
+	(854, 1112), (854, 1264),
+	(854, 1416), (854, 1568)
+]
+
+
+SONG_TEXT_POSITION = [
+	(131, 232), (131, 385),
+	(131, 537), (131, 689),
+	(131, 842), (131, 995),
+	(131, 1147), (131, 1299),
+	(131, 1452), (131, 1604)
+]
+
+ARTIST_TEXT_POSITION  = [
+	(162, 292), (162, 443),
+	(162, 594), (162, 745),
+	(162, 896), (162, 1047),
+	(162, 1198), (162, 1349),
+	(162, 1500), (162, 1651)
+]
+
+"""
+ArtistStory pixel positions
+"""
+
+ARTIST_PHOTO_POSITION  = [
+	(281, 288), (548, 288),
+	(281, 555), (548, 555),
+	(281, 822), (548, 822),
+	(281, 1089), (548, 1089),
+	(281, 1356), (548, 1356)
+]
---
+++ test_named_tuple.py
@@ -0,0 +1,77 @@
+from collections import namedtuple
+import json
+import requests
+
+
+
+ArtistData = namedtuple('ArtistStory', ['artists', 'images'])
+SongData = namedtuple('SongStory', ['songs', 'artists', 'images'])
+
+
+def create_artist_story_data():
+
+	with open('artists.json') as f:
+		data = json.load(f)
+
+	artists = [artist["name"] for artist in data["items"]]
+	images = [data["items"][i]["images"][0]['url'] for i in range(len(data["items"]))]
+
+	# more readable to parse image uris from json
+	# for item in data["items"]:
+		# uri = (item["images"][0]['url'])
+
+
+	for i, url in enumerate(images):
+		url_id_truncated = url[-8:-1]
+		request = requests.get(url).content
+		with open(f'test_downloads/artist/{url_id_truncated}.jpg', 'wb') as handler:
+			images[i] = f'test_downloads/artist/{url_id_truncated}.jpg'
+			handler.write(request)
+
+	story = ArtistData(artists=artists, images=images)
+	return story
+
+
+
+def create_song_story_data():
+
+	with open('songs.json') as f:
+			data = json.load(f)
+
+	songs = [data["items"][i]["name"] for i in range(10)]
+	artists = []
+	images = []
+
+	# ????????
+
+	for item in data["items"]:
+		_artists = item["artists"]
+		if len(_artists) > 1:
+			multi_artist_string = ", ".join([str(_artists[i]['name']) for i in range(len(_artists))])
+			artists.append(multi_artist_string)
+		else:
+			artists.append(item['artists'][0]['name'])
+
+		images.append(item["album"]["images"][0]["url"])
+
+
+	for i, url in enumerate(images):
+			url_id_truncated = url[-8:-1]
+			request = requests.get(url).content
+			with open(f'test_downloads/song/{url_id_truncated}.jpg', 'wb') as handler:
+				images[i] = f'test_downloads/song/{url_id_truncated}.jpg'
+				handler.write(request)
+
+
+	story = SongData(songs=songs, artists=artists, images=images)
+	return story
+
+
+s = create_song_story_data()
+a = create_artist_story_data()
+# print(s)
+# print(" ")
+# print(a)
+
+
+
