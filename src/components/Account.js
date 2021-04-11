f1cc220eb3a6fc690b1da5c952c8fe42b4398756
Jacob Cannon <tug33296@temple.edu>
2021-04-11 17:06:27 -0400

    update packages

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Account() {
+
+    const [account, setAccount] = useState({})
+
+    let myConfig = {
+        headers: {
+           Authorization: "Bearer " + ""
+        }
+     }
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/user', myConfig)
+        .then(res => {
+            console.log(res.data)
+            setAccount(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
     return (
         <div>
             <h1>Account Page</h1>
+            <div>{account.id}</div>
+            <div>{account.user_id}</div>
         </div>
     )
 }
