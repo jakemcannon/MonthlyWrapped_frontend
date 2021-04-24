8687b01967e03ed08f0648b36d2603c1b1fd78fa
Jacob Cannon <tug33296@temple.edu>
2021-04-24 13:45:22 -0400

    setup useState hook for filtering songs and artists

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,31 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
+const s_images = [
+    {id:1, imageName: 'december.png'},
+    {id:2, imageName: 'november.png'},
+    {id:3, imageName: 'october.png'},
+    {id:4, imageName: 'september.png'},
+    {id:5, imageName: 'august.png'},
+    {id:6, imageName: 'july.png'},
+    {id:7, imageName: 'june.png'},
+    {id:8, imageName: 'may.png'},
+    {id:9, imageName: 'april.png'},
+    {id:10, imageName: 'march.png'},
+    {id:11, imageName: 'february.png'},
+    {id:12, imageName: 'january.png'},
+]
 
-const images = [
-    {id:1, imageName: '1.png'},
-    {id:2, imageName: '2.png'},
-    {id:3, imageName: '3.png'},
-    {id:4, imageName: '4.png'},
-    {id:5, imageName: '5.png'},
-    {id:6, imageName: '6.png'},
-    {id:7, imageName: '7.png'},
-    {id:8, imageName: '8.png'},
-    {id:9, imageName: '9.png'},
-    {id:10, imageName: '10.png'},
-    {id:11, imageName: '11.png'},
-    {id:12, imageName: '12.png'},
+const a_images = [
+    {id:1, imageName: 'january.png'},
+    {id:2, imageName: 'february.png'},
+    {id:3, imageName: 'march.png'},
+    {id:4, imageName: 'april.png'},
+    {id:5, imageName: 'may.png'},
+    {id:6, imageName: 'june.png'},
+    {id:7, imageName: 'july.png'},
+    {id:8, imageName: 'august.png'},
+    {id:9, imageName: 'september.png'},
+    {id:10, imageName: 'october.png'},
+    {id:11, imageName: 'november.png'},
+    {id:12, imageName: 'december.png'},
 ]
 
 function Content() {
+
+    const [data, setData] = useState(s_images)
+
     return (
         <div>
             <div className="menu">
                 <h1 className="menu-header">Your stories</h1>
-                <Button className="menu-btn" variant="success shadow-none"> Top Songs </Button>
-                <Button className="menu-btn" variant="success shadow-none"> Top Artists </Button>
+                <Button onClick={() => setData(s_images)}  className="menu-btn" variant="success shadow-none"> Top Songs </Button>
+                <Button onClick={() => setData(a_images)} className="menu-btn" variant="success shadow-none"> Top Artists </Button>
             </div>
             <div className="image-grid">
-            {images.map(image => 
+            {data.map(image => 
                 <div key={image.id}>
                     <img class="grid-image" src={`./song_images/${image.imageName}`} alt="image"/>
                 </div>  
