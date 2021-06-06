133a9b0efff77c349926c766b302c9c2e070ce50
Jacob Cannon <tug33296@temple.edu>
2021-06-05 22:23:27 -0400

    make request to Spotify api and generate images

--- app/routes/generate_images.py
+++ app/routes/generate_images.py
@@ -1,10 +1,19 @@
 import boto3
 from flask import Flask, current_app, jsonify
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+from .test_named_tuple import make_api_request
+from .story import SongStory
+from .story import ArtistStory
+
+from models import db
+from models import User
 
 app = current_app
 
 @app.route("/generate_images")
+@jwt_required()
 def get_end_of_month_images():
 
 	# fetch the current user
@@ -13,6 +22,13 @@ def get_end_of_month_images():
 	u = User.query.filter_by(user_id=user['user_id']).first()
 
 	# build those two images
+	# TODO: throw exception if api response is bad
+	response = make_api_request(u.access_token)
+
+	s = SongStory(response[0].artists, response[0].songs, response[0].images).create_image()
+	a = ArtistStory(response[1].artists, response[1].images).create_image()
+	# a.create_image()
+	# s.create_image()
 
 	# upload those two images to S3
 
