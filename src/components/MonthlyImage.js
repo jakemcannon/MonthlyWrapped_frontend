8cd7861db1260c057fd4ffbc776ebda1d51a94da
Jacob Cannon <tug33296@temple.edu>
2021-09-04 23:18:16 -0400

    update current_month page on frontend

--- src/components/MonthlyImage.js
+++ src/components/MonthlyImage.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Auth from '../utils/Auth.js'
-import song_image from '../example_songs.png'
-import artist_image from '../example_artists.png'
 import { css } from "@emotion/react";
 import BeatLoader from "react-spinners/BeatLoader";
 
@@ -14,17 +12,22 @@ const override = css`
 
 function MonthlyImage() {
     const [isLoading, setLoading] = useState(true);
-    const [currentMonth, setCurrentMonth] = useState([])
+    const [currentMonth, setCurrentMonth] = useState(true);
+    const [images, setImages] = useState([])
 
     useEffect(() => {
         const token = Auth.get()
-        axios.get('http://127.0.0.1:5000/generate_images_test', {
+        axios.get('http://127.0.0.1:5000/current_month', {
             headers: {'Authorization': token}
         })
         .then(res => {
-            // I think we need some logic here to determine if we got image urls back or general 200 response as a background job
-            // SEE COMMENT IN PYHTON CODE
-            setCurrentMonth(res.data)
+            setImages(res.data)
+            if(res.data[0]["curMonth"]) {
+                setCurrentMonth(true)
+            } else {
+                setCurrentMonth(false)
+            }
+            setImages(res.data)
             setLoading(false);
         })
         .catch(err => {
@@ -48,18 +51,21 @@ function MonthlyImage() {
         <div className="heading">
             <BeatLoader className="spinner" color={"#76CE53"} loading={isLoading} css={override} size={20} margin={15} />  
         </div>
-         {!isLoading  && 
+         {!isLoading && currentMonth &&
          <div>
             <h1 className="heading">Here are your June 2021 stories</h1>    
              <ul className="photo-list">
                  <li>
-                    <img className="photo-list-image" src={currentMonth[0].song} alt=""/>
+                    <img className="photo-list-image" src={images[1].song} alt=""/>
                  </li>
                  <li>
-                    <img className="photo-list-image" src={currentMonth[1].artist} alt=""/>
+                    <img className="photo-list-image" src={images[2].artist} alt=""/>
                  </li>
              </ul>
         </div>}
+        {!currentMonth &&
+            <h1 className="heading">Your stories are still being processed, please check back again soon!</h1>
+        }
          </div>
     )
 }
